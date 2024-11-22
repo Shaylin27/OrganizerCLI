ec478075b532dd51d8c7bb112865a0b72c53e044
Shaylin27 <shaylingovender27@gmail.com>
2024-11-22 12:18:50 +0200

    Reworked the folder system, so it can be modified by the user by changing the config in a JSON file called options.json

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
+import json
 from pathlib import Path
 import shutil
 
 path = Path.home() / "Downloads"
 
-types = ['Docs', 'Videos', 'Pictures', 'Others']
+options = json.load(open('options.json', 'r'))
 
-for folder in types:
-    (path / folder).mkdir(exist_ok=True)
+for i, _ in options.items():
+    (path / i).mkdir(exist_ok=True)
 
 files = [f for f in path.iterdir() if f.is_file()]
 
 for file in files:
-    ext = file.suffix.lower()
-
-    match ext:
-        case '.pdf':
-            shutil.move(str(file), str(path / 'Docs' / file.name))
-        case '.mp4':
-            shutil.move(str(file), str(path / 'Videos' / file.name))
-        case '.jpg':
-            shutil.move(str(file), str(path / 'Pictures' / file.name))
-        case _:
-            shutil.move(str(file), str(path / 'Others' / file.name))
+    ext = file.suffix.lower()[1:]
+    for category in options:
+        if ext in options[category]:
+            # Leaving these debug statements here incase someone wants to under this better
+            # print(f'{ext=}, {category=}')
+            # print(f'{str(file)}')
+            shutil.move(str(file), str(path / category / file.name))

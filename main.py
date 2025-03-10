fc1b4f9922c7f35cdc1a8c5dae203c1e0248caca
Shaylin27 <90847714+Shaylin27@users.noreply.github.com>
2025-03-10 09:07:00 +0200

    Added Copy functionality along with CL argument

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Process user flags.")
 
 #TODO FIX THIS THING
-#parser.add_argument("--copy", action="store_true", help="Copy instead of moving")
+parser.add_argument("--copy", action="store_true", help="Copy instead of moving")
 #parser.add_argument("-c", "--confirm", action="store_true", help="Require confirmation before proceeding")
 #TODO Modify the code to allow the processing of other files
 #parser.add_argument("-o", "--other-files", action="store_true", help="Include other files in operation")
@@ -15,6 +15,7 @@ parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose
 parser.add_argument("-vs", "--verbose-small", action="store_true", help="Enable smaller verbose output")
 
 args = parser.parse_args()
+print(args)
 
 input_path = Path(input("Please input the path to organise:"))
 #region Loading the JSON and creating the folders
@@ -30,9 +31,15 @@ for file in files:
     extensions = file.suffix.lower()[1:]
     for category in options:
         if extensions in options[category]:
-            if args.verbose:
-                print(f'Moving "{str(file)}" to "{str(input_path / category / file.name)}"')
-            elif args.vebose_small:
-                print(f'Moving "{file.name}"')
-            # TODO add this back for final release
-            shutil.move(str(file), str(input_path / category / file.name))
+            if args.copy:
+                if args.verbose:
+                    print(f'Copying "{str(file)}" to "{str(input_path / category / file.name)}"')
+                elif args.vebose_small:
+                    print(f'Copying "{file.name}"')
+                shutil.copy(str(file), str(input_path / category / file.name))
+            else:
+                if args.verbose:
+                    print(f'Moving "{str(file)}" to "{str(input_path / category / file.name)}"')
+                elif args.vebose_small:
+                    print(f'Moving "{file.name}"')
+                shutil.move(str(file), str(input_path / category / file.name))
